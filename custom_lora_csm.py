eabefdff444105bf46bf27fc1a2ef08b6b8e5259
Kenenbek R <kenenbek@gmail.com>
2025-09-15 23:40:48 +0600

    qlora

--- custom_lora_csm.py
+++ custom_lora_csm.py
@@ -61,6 +61,8 @@ class CSMDataset(Dataset):
         item = self.data_items[idx]
         audio = item.load_audio(self.sample_rate)
         print("audio have shape: ", audio.shape)
+        print(self.processor.feature_extractor)
+        print(self.processor.tokenizer)
         inputs = self.processor(
             text=[f"<|begin_of_text|>[{item.speaker_id}]{item.text}<|end_of_text|><|AUDIO|><|audio_eos|>"],
             audio=audio,
